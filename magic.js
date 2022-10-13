fb9c975882b3b91abbcfbc09c5e15158a0e9b1b7
minsoo-web <zlemzlem5656@naver.com>
2022-10-13 18:48:44 +0900

    feat: 음악 재생 기능 추가

--- magic.js
+++ magic.js
@@ -1,31 +1,37 @@
 window.onload = () => {
-  const surface = document.querySelector('.surface');
-  const car = document.querySelector('.car');
-  const img = document.querySelector('.car img');
+  const surface = document.querySelector(".surface");
+  const car = document.querySelector(".car");
+  const img = document.querySelector(".car img");
+  const audio = document.querySelector(".audio");
   let flag = false;
 
-  const cars = ['./assets/Img_06.png', './assets/Img_05.png'];
+  const cars = ["./assets/Img_06.png", "./assets/Img_05.png"];
 
-  document.addEventListener('keypress', e => {
-    if (e.code === 'KeyD') {
+  document.addEventListener("keypress", e => {
+    if (e.code === "KeyD") {
       if (flag) {
-        surface.classList.toggle('moveRight');
-        car.classList.toggle('suspension');
+        surface.classList.toggle("moveRight");
+        car.classList.toggle("suspension");
       } else {
-        alert('운전하기 전에 잊으신 게 없나요?');
+        alert("운전하기 전에 잊으신 게 없나요?");
       }
     }
 
-    if (e.code === 'KeyL') {
+    if (e.code === "KeyL") {
       if (flag) {
         flag = false;
-        img.setAttribute('src', cars[1]);
-        surface.classList.remove('moveRight');
-        car.classList.remove('suspension');
+        img.setAttribute("src", cars[1]);
+        surface.classList.remove("moveRight");
+        car.classList.remove("suspension");
       } else {
         flag = true;
-        img.setAttribute('src', cars[0]);
+        img.setAttribute("src", cars[0]);
       }
+    }
+
+    if (e.code === "KeyP") {
+      audio.volume = 0.2;
+      audio.play();
     }
   });
 };
